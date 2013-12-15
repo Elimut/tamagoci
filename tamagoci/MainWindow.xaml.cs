06aa304d50c51f3fbe315fbb27f490c758dfee47
Eliska Vlckova <eliska.vlckova03@upol.cz>
2013-12-15 12:18:20 +0100

    zalozeni tamagoci do Git repa

---
+++ tamagoci/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+// http://www.wpf-tutorial.com/misc-controls/the-progressbar-control/  <--- procenta na progress baru
+
+
+namespace tamagoci
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Zviratko pet;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.pet = new Zviratko(this);
+            this.progress_play.Value = this.pet.Played;
+            this.progress_eat.Value = this.pet.Eaten;
+            this.progress_sleep.Value = this.pet.Slept;
+        }
+
+        private void button_exit_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Environment.Exit(0);
+            this.Close();
+        }
+
+        private void progress_play_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+           //ProgressBar Foreground = "LightGreen";
+            ProgressBar pplay = sender as ProgressBar;
+           // this.progress_eat.Value = pplay.Value;
+           this.pet.Played = pplay.Value;
+        }
+
+        private void progress_eat_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ProgressBar peat = sender as ProgressBar;
+            this.pet.Eaten = peat.Value;
+        }
+
+        private void progress_sleep_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ProgressBar psleep = sender as ProgressBar;
+            this.pet.Slept = psleep.Value;
+        }
+
+        private void do_play_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            this.progress_play.Value += 1;
+        }
+
+        private void do_eat_Click(object sender, RoutedEventArgs e)
+        {
+            this.progress_eat.Value += 1;
+        }
+
+        private void do_sleep_Click(object sender, RoutedEventArgs e)
+        {
+            this.progress_sleep.Value += 1;
+        }
+
+    }
+}
---
+++ tamagoci/Zviratko.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace tamagoci
+{
+    class Zviratko 
+    {
+        private MainWindow mw;
+#if TIMER
+        private Timer timer; //mřížka define TIMER
+#else
+        private DispatcherTimer timer; //běží toto
+#endif
+        private double played;
+        private double eaten;
+        private double slept;
+        public Zviratko(MainWindow mw) {
+          
+            played = 50;
+            eaten = 10;
+            slept = 90;
+            this.mw = mw;
+
+            #if TIMER
+            timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            timer.Interval = 500;
+            timer.Enabled = true; //totéž co Start()
+
+            #else
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(OnTimedEvent);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+            #endif
+        }
+        private void OnTimedEvent(object source, /*Elapsed*/EventArgs e)
+        {
+            this.played -= 1;
+            this.eaten -=  1;
+            this.slept -= 1;
+
+            if (played > 0 && eaten > 0 && slept > 0)
+            {
+                this.mw.progress_play.Value = this.Played;
+                this.mw.progress_eat.Value = this.Eaten;
+                this.mw.progress_sleep.Value = this.Slept;
+            }
+            else
+            {
+               MessageBoxResult result = MessageBox.Show(
+                   "Animal is dead. Do you want to quit?",
+                   "OK",
+                       MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.No);
+               if (result == MessageBoxResult.Yes) {
+                   mw.Close();
+               }
+            }
+        }
+
+        public double Played
+        {
+            get
+            {
+                return this.played;
+            }
+
+            set
+            {
+                this.played = value;
+            }
+        }
+        public double Eaten
+        {
+            get
+            {
+                return this.eaten;
+            }
+
+            set
+            {
+                this.eaten = value;
+            }
+        }
+        public double Slept
+        {
+            get
+            {
+                return this.slept;
+            }
+
+            set
+            {
+                this.slept = value;
+            }
+        }
+
+    }
+}
